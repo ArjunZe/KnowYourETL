4bf201597c4fa87e6fc5a1fadef1924030b22f0f
ArjunZe <zeroerror@live.in>
2020-08-22 21:20:35 +0530

    v1

---
+++ src/main/java/com/infa/knowyouretl/ClientMultiThreadedExecution.java
@@ -0,0 +1,261 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * https://hc.apache.org/httpcomponents-client-ga/httpclient/examples/org/apache/http/examples/client/ClientMultiThreadedExecution.java
+ * https://www.mrityunjay.com/index-and-search-structured-xml-documents-using-apache-solr/
+ */
+package com.infa.knowyouretl;
+
+import com.google.common.collect.Lists;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import org.apache.commons.io.FileUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ *
+ * @author mreddy
+ */
+public class ClientMultiThreadedExecution {
+
+    // static vars
+    public static String serverUrl;
+    public static String icSessionId;
+    public static String uname;
+    public static String passwd;
+    public static String hostname;
+    public static String podID = null;
+    private static HttpRequestResponse sHrr;
+    private static Map<String, String> sHeaders = new HashMap<String, String>();
+    private static Map<String, String> mctMapList = new HashMap<>();
+    private static List<String> mctIdList;
+
+    static {
+        sHeaders.put("Accept", "application/xml");
+    }
+
+    public static void main(String[] args) throws IOException {
+    
+        if (args.length < 2 ) {
+            System.out.println("to run -> java -jar knowYourETL.jar <IICSusername> <IICSpassword> <hostname like dm-us.informaticacloud.com>");
+            System.exit(0);
+        }
+        
+        ClientMultiThreadedExecution.mctIdList = new ArrayList();
+        ClientMultiThreadedExecution.login();
+        ClientMultiThreadedExecution.getMttList();
+        //https://stackoverflow.com/questions/23920425/loop-arraylist-in-batches
+        //final List<String> listToBatch = new ArrayList<>();
+        //listToBatch.add("test");
+        final List<List<String>> batch = Lists.partition(ClientMultiThreadedExecution.mctIdList, 10);
+        batch.forEach((list) -> {
+            // Add your code here
+            // list.forEach((mcid) -> {
+            //   System.out.println("calling httpts:/getmci:1111/" + mcid);
+            //});
+            try {
+                ClientMultiThreadedExecution.devideNconcur(list);
+            } catch (IOException ioe) {
+                System.out.println(" - error: " + ioe);
+            } catch (InterruptedException ex) {
+                java.util.logging.Logger.getLogger(ClientMultiThreadedExecution.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+        //
+    }
+
+    public static void devideNconcur(List<String> batch) throws IOException, InterruptedException {
+         // Create an HttpClient with the ThreadSafeClientConnManager.
+        // This connection manager must be used if more than one thread will
+        // be using the HttpClient.
+        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+        //DO NOT SET MaxTotal to more than 10.
+        cm.setMaxTotal(10);
+        try (CloseableHttpClient httpclient = HttpClients.custom()
+                .setConnectionManager(cm)
+                .build()) {
+            // create an array of URIs to perform GETs on
+            List<String> mctURI=new ArrayList();
+            batch.forEach((mcid)->{
+                    mctURI.add(ClientMultiThreadedExecution.serverUrl + "/api/v2/mttask/"+mcid);
+            });
+            /*String[] urisToGet = {
+                ClientMultiThreadedExecution.serverUrl + "/api/v2/mttask/",
+                "https://extendsclass.com/mock/rest/d2af925eee064f3518279e8b1d1ed1ce/ns",
+                "https://extendsclass.com/mock/rest/d2af925eee064f3518279e8b1d1ed1ce/ns",};
+            */
+            // create a thread for each URI
+            GetThread[] threads = new GetThread[mctURI.size()];
+            for (int i = 0; i < mctURI.size(); i++) {
+                HttpGet httpget = new HttpGet(mctURI.get(i));
+                threads[i] = new GetThread(httpclient, httpget, i + 1);
+            }
+            // start the threads
+            for (GetThread thread : threads) {
+                thread.start();
+            }
+            // join the threads
+            for (GetThread thread : threads) {
+                thread.join();
+            }
+        }
+    }
+
+    /**
+     * A thread that performs a GET.
+     */
+    static class GetThread extends Thread {
+
+        private final CloseableHttpClient httpClient;
+        private final HttpContext context;
+        private final HttpGet httpget;
+        private final int id;
+
+        public GetThread(CloseableHttpClient httpClient, HttpGet httpget, int id) {
+            this.httpClient = httpClient;
+            this.context = new BasicHttpContext();
+            this.httpget = httpget;
+            this.id = id;
+        }
+
+        /**
+         * Executes the GetMethod and prints some status information.
+         */
+        @Override
+        public void run() {
+            try {
+                System.out.println("Thread-"+ id + ": getting XML using " + httpget.getURI());
+                String[] urlToekns=httpget.getURI().toString().split("/");
+                httpget.addHeader("Accept", "Application/xml");
+                httpget.addHeader("icsessionid", ClientMultiThreadedExecution.icSessionId);
+                try (CloseableHttpResponse response = httpClient.execute(httpget, context)) {
+                    System.out.println("Thread-"+id + ": received XML for "+ urlToekns[urlToekns.length-1]);
+                    // get the response body as an array of bytes
+                    // HttpEntity entity = response.getEntity().
+                    String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+                    System.out.println("Thread-"+ id + ": writing mttXml of " + urlToekns[urlToekns.length-1]);
+                    File file = new File(urlToekns[urlToekns.length-1] + ".xml");
+                    FileUtils.writeStringToFile(file, responseBody);
+                    //https://www.tutorialspoint.com/jsoup/jsoup_parse_body.htm
+                    /*if (entity != null) {
+                        byte[] bytes = EntityUtils.toByteArray(entity);
+                        System.out.println(id + " - " + bytes.length + " bytes read");
+                        System.out.println(id + " - Response body: " + responseBody);
+                    }*/
+                }
+            } catch (IOException e) {
+                System.out.println(id + " - error: " + e);
+            }
+        }
+
+    }
+
+    public static void login() {
+        String payload;
+        int code;
+        JSONObject obj = new JSONObject();
+        obj.put("@type", "login");
+        obj.put("username", uname);
+        obj.put("password", passwd);
+        payload = obj.toString();
+
+        String line;
+        StringBuilder jsonString = new StringBuilder();
+        try {
+            //http://stackoverflow.com/questions/15570656/how-to-send-request-payload-to-rest-api-in-java
+            URL url = new URL("https://" + hostname+ "/ma/api/v2/user/login");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setRequestProperty("Content-Type", "application/json;");
+            try (OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8")) {
+                writer.write(payload);
+            }
+            code = connection.getResponseCode();
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getResponseCode() / 100 == 2 ? connection.getInputStream() : connection.getErrorStream()))) {
+                while ((line = br.readLine()) != null) {
+                    jsonString.append(line);
+                }
+            }
+            connection.disconnect();
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        String s = jsonString.toString();
+        JSONObject jsonObject = new JSONObject(s);   //http://stackoverflow.com/questions/16574482/decoding-json-string-in-java
+        if (code != 200) {
+            String e = jsonObject.getString("@type");
+            if (e.equals("error")) {
+                System.out.println(jsonObject.getString("description"));
+                System.exit(0);
+            }
+        }
+        ClientMultiThreadedExecution.icSessionId = jsonObject.getString("icSessionId");
+        ClientMultiThreadedExecution.serverUrl = jsonObject.getString("serverUrl");
+        System.out.print("token & url " + jsonObject.getString("icSessionId") + "," + jsonObject.getString("serverUrl"));
+
+    }
+
+    public static void getMttList() {
+        sHrr = new HttpRequestResponse();
+        sHrr.setMethod("GET");
+        sHeaders.put("icsessionid", ClientMultiThreadedExecution.icSessionId);
+        sHrr.setHeaders(sHeaders);
+        sHrr.setUrl(ClientMultiThreadedExecution.serverUrl + "/api/v2/mttask");
+        sHrr.makeCall();
+        if (sHrr.getUrlStatus() == 200) {
+            String mtList = sHrr.getResponse();
+            try {
+                Document document = Jsoup.parseBodyFragment(mtList);
+                Element body = document.body();
+                //Elements aList = document.select("mtTask");
+                Elements paragraphs = body.getElementsByTag("id");
+                paragraphs.forEach((paragraph) -> {
+                    ClientMultiThreadedExecution.mctIdList.add(paragraph.text());
+                });
+                /* logger.info(aList);
+                if (aList.size() <= 0) {
+                //System.out.print("emprty");
+                } else {
+                aList.forEach((eachAgentInfo) -> {
+                mctMapList.put(eachAgentInfo.id(), eachAgentInfo.attr("href").split("'")[1]);
+                });
+                }*/
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+            }
+
+        } else {
+            //AlertWindow.error(sHrr.getErrorString());
+            System.out.print(String.format("Login failed : HTTP status = %d", (int) sHrr.getUrlStatus()));
+        }
+    }
+
+}
---
+++ src/main/java/com/infa/knowyouretl/HttpRequestResponse.java
@@ -0,0 +1,295 @@
+package com.infa.knowyouretl;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * <h1> HttpRequestResponse </h1>
+ * <p>
+ * This class will send the request to the server and get the response back from
+ * the server.</br>
+ * This class uses {@link java.net.HttpURLConnection} object to establish the
+ * connection to an end-point server.</br>
+ * Also multiple calls can be made from the same "HttpRequestResponse" object as
+ * the buffer is flushed everytime before the call is made to the server.</br>
+ * </br>
+ * <b>[Note : If the server request fails with 403 or some other codes then
+ * response data will not be stored.</b></p>
+ *
+ * <p>
+ * Below is the example </p>
+ * <pre>
+ * {@code
+ *  HttpRequestResponse hrr = new HttpRequestResponse();
+ *  hrr.setUrl("https://www.google.com");	//Set the URL
+ *  hrr.setMethod("GET");					//Set the method GET/POST
+ *
+ *  Map<String, String> headers = new HashMap<String,String>(); // Create a map and add headers in key-value pair
+ *  headers.put("Accept","text/html");
+ *
+ *  hrr.setHeaders(headers);				//Set the headers
+ *  hrr.makeCall();							//Call endpoint server.
+ *
+ *  if(hrr.getUrlStatus()==200)				//Check the response code from the server.
+ *  {
+ *  	System.out.println(hrr.getResponse());	// Print the output
+ *  }
+ *
+ * }
+ * </pre>
+ *
+ * @author Kartik
+ * @version 1.0
+ */
+public class HttpRequestResponse implements AutoCloseable {
+
+    private final int CONNECTION_TIMEOUT = 180 * 1000;
+    private short urlStatus;
+    private String method;
+    private Map<String, String> headers = new HashMap<String, String>();
+    private StringBuffer response = new StringBuffer();
+    private String requestData;
+    private String url;
+    private StringBuffer errorMessage = new StringBuffer();
+    HttpURLConnection connection = null;
+
+    /**
+     * <p>
+     * The HttpRequestResponse object is instantiated.</p>
+     */
+    public HttpRequestResponse() {
+    }
+
+    /**
+     * <p>
+     * This method returns the method type of the request "GET"/"POST" set by
+     * {@link #setMethod(String)} method</p>
+     *
+     * @return method "GET"/"POST"
+     */
+    public String getMethod() {
+        return method;
+    }
+
+    /**
+     * <p>
+     * This method sets the request type "GET"/"POST" accordingly.</p>
+     *
+     * @param method "GET"/"POST"
+     */
+    public void setMethod(String method) {
+        System.out.println(String.format("Method type : %s", method));
+        this.method = method;
+    }
+
+    /**
+     * <p>
+     * This method returns the request headers set by {@link #setHeaders(Map)}
+     * method.</p>
+     *
+     * @return headers Headers in key-value pair
+     */
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    /**
+     * <p>
+     * This method sets the request headers. </p>
+     * Example
+     * <pre>
+     * {@code
+     *
+     * HttpRequestResponse hrr = new HttpRequestResponse();
+     * hrr.setUrl("https://www.google.com");
+     * hrr.setMethod("GET");
+     *
+     * Map<String, String> headers = new HashMap<String,String>();
+     * headers.put("Accept","text/html");
+     *
+     * hrr.setHeaders(headers); // This will set the request headers.
+     *
+     * }
+     *
+     * </pre>
+     *
+     * @param headers Headers in key-value pair.
+     */
+    public void setHeaders(Map<String, String> headers) {
+        System.out.println("Headers : " + headers);
+        this.headers = headers;
+    }
+
+    /**
+     * <p>
+     * This method will return the data received from the server</p>
+     *
+     * @return response data received from the server
+     */
+    public String getResponse() {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String data;
+            while ((data = br.readLine()) != null) {
+                response.append(data);
+            }
+            System.out.println(String.format("Response : %s", response.toString()));
+            return response.toString();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+
+    public String getErrorString() {
+        return errorMessage.toString();
+    }
+
+    /**
+     * <p>
+     * This method will return the data sent to the server during "POST" call
+     * set by
+     * {@link com.informatica.core.HttpRequestResponse#setRequestData(String)}
+     * method.</p>
+     *
+     * @return requestData data send to the server.
+     */
+    public String getRequestData() {
+        return requestData;
+    }
+
+    /**
+     * <p>
+     * This method will copy the data to an internal buffer "requestData".</p>
+     *
+     * @param requestData Data/payload that will be sent to the server during
+     * "POST" call.
+     */
+    public void setRequestData(String requestData) {
+        System.out.println(String.format("RequestData : %s", requestData));
+        this.requestData = requestData;
+    }
+
+    /**
+     * <p>
+     * This method returns the HTTP status code.</p>
+     *
+     * @return urlStatus HTTP status code Ex : 200, 403, 500........
+     */
+    public short getUrlStatus() {
+        return urlStatus;
+    }
+
+    /**
+     * <p>
+     * This method returns the url set by
+     * {@link com.informatica.core.HttpRequestResponse#setUrl(String)}
+     * method.</p>
+     *
+     * @return url URL as String is returned if set, else null is returned.
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * <p>
+     * This method will set the URL.</p>
+     *
+     * @param url URL in String format should be provided.
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * <p>
+     * This will create a new {@link HttpURLConnection} object and opens the
+     * connection on it using new {@link URL} object.</p>
+     *
+     * <p>
+     * The response from the server is fetched using
+     * {@link HttpURLConnection#getInputStream()} method.</br>
+     * and is stored in StringBuffer and later converted to string</p>
+     *
+     */
+    public void makeCall() {
+        try {
+            // flush the buffer
+            response.delete(0, response.length());
+
+            System.out.println(String.format("Calling URL : %s", url));
+            URL u = new URL(this.url); // Create URL object
+            connection = (HttpURLConnection) u.openConnection();
+            connection.setRequestMethod(method); // Set request method
+
+            //Add headers
+            for (Entry<String, String> header : headers.entrySet()) {
+                connection.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            connection.setConnectTimeout(CONNECTION_TIMEOUT);
+
+            // check whether the method is GET/POST, if POST method is used then send the data
+            if (method.equalsIgnoreCase("POST")) {
+                connection.setDoOutput(true);
+                DataOutputStream dout = new DataOutputStream(connection.getOutputStream());
+                dout.writeBytes(requestData);
+                dout.flush();
+                dout.close();
+            }
+
+            connection.connect(); // Makes connection to endpoint.
+            urlStatus = (short) connection.getResponseCode();
+
+            if (urlStatus != 200) {
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getErrorStream()))) {
+                    errorMessage.delete(0, errorMessage.length());
+                    String data;
+                    while ((data = br.readLine()) != null) {
+                        errorMessage.append(data);
+                    }
+                    System.out.println(errorMessage.toString());
+                }
+
+            }
+            System.out.println("Call completed");
+
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+
+        }
+    }
+
+    public InputStream getResponseStream() {
+        try {
+            return connection.getInputStream();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection.getInputStream() != null) {
+            connection.getInputStream().close();
+        }
+    }
+
+}
